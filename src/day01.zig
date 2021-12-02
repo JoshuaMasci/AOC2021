f9796b2dce186561ea117580b48445f645f2c1dd
Joshua_Masci <iamshortman@gmail.com>
2021-12-01 22:31:59 -0800

    Day 1 part 1 and 2

--- src/day01.zig
+++ src/day01.zig
@@ -11,8 +11,49 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day01.txt");
 
-pub fn main() !void {
+fn read_line(buf: []const u8) !i64 {
+    var line_len = line.len;
+    if (line[line_len - 1] == 13) {
+        line_len = line_len - 1;
+    }
+    var value = try std.fmt.parseInt(i64, line[0..line_len], 10);
+    return value;
+}
 
+pub const GeneralPurposeAllocator: type = std.heap.GeneralPurposeAllocator(.{ .enable_memory_limit = true });
+
+pub fn main() !void {
+    var lines = std.mem.split(data, "\n");
+
+    var global_allocator: GeneralPurposeAllocator = GeneralPurposeAllocator{};
+    var allocator = &global_allocator.allocator;
+
+    var data_list = List(i64).init(allocator);
+    while (lines.next()) |line| {
+        var line_len = line.len;
+        if (line[line_len - 1] == 13) {
+            line_len = line_len - 1;
+        }
+        var value = try std.fmt.parseInt(i64, line[0..line_len], 10);
+        try data_list.append(value);
+    }
+
+    var prev: i64 = std.math.maxInt(i64);
+    var count: u64 = 0;
+
+    var i: u64 = 2;
+    while (i < data_list.items.len) : (i += 1) {
+        var value = data_list.items[i] + data_list.items[i - 1] + data_list.items[i - 2];
+
+        if (value > prev) {
+            count += 1;
+        }
+
+        prev = value;
+        std.log.info("Value: {} ", .{data_list.items[i]});
+    }
+
+    std.log.info("Count: {} ", .{count});
 }
 
 // Useful stdlib functions
