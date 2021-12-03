f290a8c26509004bdb4311e766f21dc9be31d880
Joshua_Masci <iamshortman@gmail.com>
2021-12-02 23:08:08 -0800

    Day 3 part 1

--- src/day03.zig
+++ src/day03.zig
@@ -11,8 +11,47 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day03.txt");
 
+pub const GeneralPurposeAllocator: type = std.heap.GeneralPurposeAllocator(.{ .enable_memory_limit = true });
+
 pub fn main() !void {
-    
+    var global_allocator: GeneralPurposeAllocator = GeneralPurposeAllocator{};
+    var allocator = &global_allocator.allocator;
+
+    var lines = List([12]bool).init(allocator);
+
+    var lines1 = std.mem.split(data, "\n");
+    while (lines1.next()) |line| {
+        var line_data: [12]bool = undefined;
+        for (line) |value, index| {
+            if (index < 12) {
+                line_data[index] = (value != 48);
+            }
+        }
+        try lines.append(line_data);
+    }
+
+    var result: [12]u8 = undefined;
+
+    var index: usize = 0;
+    while (index < 12) : (index += 1) {
+        var ones: usize = 0;
+        var zeros: usize = 0;
+        for (lines.items) |line| {
+            if (line[index]) {
+                ones += 1;
+            } else {
+                zeros += 1;
+            }
+        }
+
+        if (ones > zeros) {
+            result[index] = 1;
+        } else {
+            result[index] = 0;
+        }
+    }
+
+    std.log.info("Result: {any}", .{result});
 }
 
 // Useful stdlib functions
